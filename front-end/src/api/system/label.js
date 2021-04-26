d58758e47698589a00aa677344d45ecf2ea65bf9
tanhong <tanhong@asiainfo.com>
2021-04-27 01:14:03 +0800

    接口新增

---
+++ front-end/src/api/system/label.js
@@ -0,0 +1,45 @@
+// 获取话题列表
+import request from "@/utils/request";
+
+export function getLabelList() {
+  return request({
+    url: '/system/lable/list',
+    method: 'get'
+  })
+}
+
+
+
+// 新增话题
+export function addLabel(data) {
+  return request({
+    url: '/system/lable',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改话题
+export function changeLabel(data) {
+  return request({
+    url: '/system/lable',
+    method: 'put',
+    data: data
+  })
+}
+
+// 话题详情
+export function getLabelItem(data) {
+  return request({
+    url: '/system/lable/'+data,
+    method: 'get',
+  })
+}
+
+// 删除话题
+export function deleteLabelItem(data) {
+  return request({
+    url: '/system/lable/'+data,
+    method: 'delete',
+  })
+}
