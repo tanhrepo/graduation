586fe3867e80669267df4ac56c0db837f166a2de
dfm <1355172440@qq.com>
2021-05-04 11:52:33 +0800

    签名修改 userName

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -127,7 +127,7 @@ public class SysUserController extends BaseController
     @ApiOperation("根据userName获取详细信息")
     @PreAuthorize("@ss.hasPermi('system:user:query')")
     @GetMapping("{userName}")
-    public AjaxResult getInfo(@PathVariable(value = "userName", required = true) String userName)
+    public AjaxResult getInfoByuserName(@PathVariable(value = "userName", required = true) String userName)
     {
         SysUser user = userService.selectUserByUserName(userName);
         return AjaxResult.success(user);
