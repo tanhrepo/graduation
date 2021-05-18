abdc4223bc88fb10aabc69f36a25c5f1f56c7bb8
tanhong <tanhong@asiainfo.com>
2021-05-18 10:06:09 +0800

    代码规范

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
@@ -8,9 +8,10 @@
 export default {
   name: 'App'
 }
+
 </script>
 <style scoped>
-#app{
+#app {
   height: 100%;
 }
 </style>
